4212e21793ef73dcdd3ce61d4aca15b7e4ece625
Wessel van der Veen <wvn@synadia.nl>
2019-08-09 15:05:32 +0200

    fixes wrong version

--- main.tf
+++ main.tf
@@ -173,7 +173,7 @@ resource "aws_cloudfront_distribution" "default" {
 }
 
 module "dns" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.2.5"
+  source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.3.0"
   enabled          = var.dns_aliases_enabled
   aliases          = var.aliases
   parent_zone_id   = var.parent_zone_id
